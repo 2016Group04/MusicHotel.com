e013a7a486a55862fc430058ceffda4796cbf0d2
zyy0605 <zhangyaoyao0605@sina.com>
2016-09-21 11:13:55 +0800

    再次上传

---
+++ MusicHotel/src/com/servlet/DegisteServlet.java
@@ -0,0 +1,40 @@
+package com.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class DegisteServlet extends HttpServlet {
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		String action = request.getParameter("action");
+		if("checkAccount".equals(action)){
+			checkAccount(request,response);
+		}
+
+	}
+	
+	public void checkAccount(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String account = request.getParameter("account");
+		System.out.println(account);
+		System.out.println("检查昵称是否存在");
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		this.doGet(request, response);
+	}
+
+}
